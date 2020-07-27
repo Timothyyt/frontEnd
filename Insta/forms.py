02d0bdbf4c10ab78f6ab2a3ba644bf119a87fc26
Timothyyt <tian_yang@gwu.edu>
2020-07-27 14:47:34 -0400

    Display static page

--- Insta/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from Insta.models import InstaUser
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = InstaUser
-        fields = ('username', 'email', 'profile_pic')
--- Insta/urls.py
+++ Insta/urls.py
@@ -13,19 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
-from Insta.views import (HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, addLike, UserDetailView)
+from Insta.views import HelloWorld
 
 urlpatterns = [
-    path('helloworld', HelloWorld.as_view(), name='helloworld'),
-    path('', PostsView.as_view(), name='posts'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', PostCreateView.as_view(), name='make_post'),
-    path('posts/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
-    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
-    path('like', addLike, name='addLike'),
-    path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail')
+    path('', HelloWorld.as_view(), name='helloworld')
 
 ]
